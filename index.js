5401e3693881b6e0e29edcbe29b78a24ba13857e
Matthew Bickell <matthew@nology.io>
2020-06-04 12:28:30 +0100

    basic calculator

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.querySelector("form").addEventListener("submit", event => event.preventDefault());
+const form = document.querySelector("form");
 
 const calculate = (firstNumber, operator, secondNumber) => {
   let result;
@@ -29,3 +29,17 @@ const calculate = (firstNumber, operator, secondNumber) => {
 
   return result;
 };
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const firstNumberString = document.querySelector("#first-number").value;
+  const firstNumber = parseFloat(firstNumberString);
+  const operator = document.querySelector("#operator").value;
+  const secondNumberString = document.querySelector("#second-number").value;
+  const secondNumber = parseFloat(secondNumberString);
+
+  const result = calculate(firstNumber, operator, secondNumber);
+
+  document.querySelector("#result").innerHTML = result;
+});
